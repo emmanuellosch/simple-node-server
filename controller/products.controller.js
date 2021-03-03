592c95a1fdcefddc13d6e28fa7896a8aca8cc126
Emmanuel Losch <emmanuellosch@live.de>
2021-03-03 11:32:47 +0100

    adding db.json

--- controller/products.controller.js
+++ controller/products.controller.js
@@ -1,14 +1,33 @@
 import { v4 as uuidv4 } from "uuid";
-
-const products = [];
+import fs from "fs";
 
 function postProduct(request, response) {
   const newProduct = { ...request.body, id: uuidv4() };
-  products.push(newProduct);
-  response.json(newProduct);
+
+  fs.readFile("db.json", (error, fileContents) => {
+    if (error) {
+      console.error(error.message);
+    }
+
+    const database = JSON.parse(fileContents);
+    const products = database.products;
+    products.push(newProduct);
+
+    fs.writeFile("db.json", JSON.stringify(database, null, 2), (error) => {
+      if (error) {
+        console.error(error.message);
+        response.json({ error: error.message });
+        return;
+      }
+      response.json(newProduct);
+    });
+  });
 }
 
 function getProducts(request, response) {
+  fs.readFile("db.json", (error, fileContents) => {
+    const database = JSON.parse(fileContents);
+  });
   response.json({ products });
 }
 
